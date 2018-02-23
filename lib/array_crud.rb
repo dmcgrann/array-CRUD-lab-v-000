c983e92abe333e8c421c8d0eefd371306fbf7c3d
David McGrann <dpmcgrann@gmail.com>
2018-02-23 23:14:38 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -20,8 +20,8 @@ end
 
 def remove_element_from_end_of_array(array)
   array = []
-  element = "wow"
-  array.pop("arrays!")
+  element = ""
+  arrays! = array.pop
 end
 
 def remove_element_from_start_of_array(array)
