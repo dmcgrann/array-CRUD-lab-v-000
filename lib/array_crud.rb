f952b9b3e6c5de2648d2aeb080d7d0db37615c90
David McGrann <dpmcgrann@gmail.com>
2018-02-23 23:20:05 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,15 +11,11 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  array = []
-  element = "wow"
   array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  array = []
-  element = ""
-  arrays = [].pop
+  arrays = array.pop
 end
 
 def remove_element_from_start_of_array(array)
